11be17d711bc4b9605c0d314fa47bc8e4b824e30
First Last <salmanashraf853@gmail.com>
2019-10-12 08:02:05 +0100

    Updates

---
+++ precache-manifest.6ca62dd4f4d1ab4f5235322df9a6c5b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a2a55601858da693714fb3abc12004",
+    "url": "/FPTI/index.html"
+  },
+  {
+    "revision": "75f72cceebe84f28ff1b",
+    "url": "/FPTI/static/css/2.366af2a3.chunk.css"
+  },
+  {
+    "revision": "65adeae42600833111df",
+    "url": "/FPTI/static/css/main.938fda9c.chunk.css"
+  },
+  {
+    "revision": "75f72cceebe84f28ff1b",
+    "url": "/FPTI/static/js/2.5110b045.chunk.js"
+  },
+  {
+    "revision": "65adeae42600833111df",
+    "url": "/FPTI/static/js/main.51a164a9.chunk.js"
+  },
+  {
+    "revision": "79f90d8fd38af56bbccb",
+    "url": "/FPTI/static/js/runtime~main.a6cdf523.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FPTI/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FPTI/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FPTI/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FPTI/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FPTI/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
