d221d9951d025aec39e6ef7846fc784b203ba76b
First Last <salmanashraf853@gmail.com>
2019-10-21 09:37:03 +0100

    Updates

--- precache-manifest.487caeda0c0cef9e5b6eb65301983be1.js
+++ precache-manifest.487caeda0c0cef9e5b6eb65301983be1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c7e8c42f4a3036b27bb5b3cbe845d08b",
+    "revision": "36dc504c2ac4098e0cc2dc494ab5448c",
     "url": "/FPTI/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FPTI/static/css/2.366af2a3.chunk.css"
   },
   {
-    "revision": "652b92eedc3a0293b2ee",
-    "url": "/FPTI/static/css/main.6440cc46.chunk.css"
+    "revision": "1a310e69a4ca3a278329",
+    "url": "/FPTI/static/css/main.5ef6dc15.chunk.css"
   },
   {
     "revision": "8a4dc25b6dd9296652a1",
     "url": "/FPTI/static/js/2.89a8f264.chunk.js"
   },
   {
-    "revision": "652b92eedc3a0293b2ee",
-    "url": "/FPTI/static/js/main.21dece4b.chunk.js"
+    "revision": "1a310e69a4ca3a278329",
+    "url": "/FPTI/static/js/main.0a34b5d4.chunk.js"
   },
   {
     "revision": "79f90d8fd38af56bbccb",
